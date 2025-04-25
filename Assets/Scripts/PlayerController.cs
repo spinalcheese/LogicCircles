1e763144aabd069064297254abd97c1b770eb6f8
cheez <alexedmayo@gmail.com>
2025-04-25 17:00:21 -0400

    game is now running in a perfect loop

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,17 +5,44 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public bool right;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (!right)
+        {
+            transform.Rotate(Vector3.back * 180);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.right * speed);
-        transform.Rotate(Vector3.forward);//back is right, forward is left
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DirectionalNode dn = other.GetComponent<DirectionalNode>();
+        Debug.Log(dn.current);
+
+        //check directions
+        float rot = 0;
+        Vector3 dir = Vector3.zero;
+        if (dn.current == "right")
+        {
+            dir = Vector3.back;
+            rot = 90;
+        }
+        else if (dn.current == "left")
+        {
+            dir = Vector3.forward;
+            rot = 90;
+        }
+
+
+            transform.Rotate(dir * rot);//back is right, forward is left
     }
 }
