ec441c85bc117aad9cb61249600903d72cbf685f
cheez <alexedmayo@gmail.com>
2025-04-25 15:23:36 -0400

    added vectors of right and forward

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * speed);
+        transform.Rotate(Vector3.forward);//back is right, forward is left
+    }
+}
